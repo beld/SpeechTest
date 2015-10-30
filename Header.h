a68e2401194c1cd9d9b5ace8f1a10c46de1c221f
Ding Liu <ding.liu@tum.de>
2015-10-31 00:34:51 +0100

    Test OpenEars framework

---
+++ Header.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  SpeechTest
+//
+//  Created by Ding on 10/30/15.
+//  Copyright © 2015 TUM LS1. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OEEventsObserver.h>
+
+#endif /* Header_h */
---
+++ SpeechTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SpeechTest
+//
+//  Created by Ding on 10/30/15.
+//  Copyright © 2015 TUM LS1. All rights reserved.
+//
+
+import UIKit
+
+var lmPath: String!
+var dicPath: String!
+var words: Array<String> = []
+var currentWord: String!
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+
+}
+
